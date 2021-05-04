a99c2fc4b0b2f03e846734eae606e430815173f8
Love O'tudor <loveotudor@gmail.com>
2021-05-04 21:29:14 +0100

    Implemented Authentication Screens.

---
+++ app/src/main/java/com/android/asa/ui/auth/AuthenticationActivity.java
@@ -0,0 +1,16 @@
+package com.android.asa.ui.auth;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+}
