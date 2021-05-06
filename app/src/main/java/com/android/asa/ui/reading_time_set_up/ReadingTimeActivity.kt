8e848806c1c7a10a1dc9152bdb21ff13cec199de
Love O'tudor <loveotudor@gmail.com>
2021-05-06 06:47:41 +0100

    Implemented navigation architecture in AuthenticationActivity.kt and ReadingTimeActivity.kt

---
+++ app/src/main/java/com/android/asa/ui/reading_time_set_up/ReadingTimeActivity.kt
@@ -0,0 +1,15 @@
+package com.android.asa.ui.reading_time_set_up
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.asa.R
+
+class ReadingTimeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reading_time)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+}
