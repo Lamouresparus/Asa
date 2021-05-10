5f3fc62c75c780e6d2f21eb0f7341f362bb21017
Love O'tudor <loveotudor@gmail.com>
2021-05-10 21:11:42 +0100

    basic set up for reading progress chart

--- app/src/main/java/com/android/asa/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android/asa/ui/home/HomeFragment.kt
@@ -5,21 +5,77 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.android.asa.databinding.FragmentHomeBinding
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
+import javax.xml.datatype.DatatypeConstants.DAYS
+
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?,
+    ): View {
         binding = FragmentHomeBinding.inflate(layoutInflater)
 
-        findNavController().navigate(BeginSemesterFragmentDirections.actionBeginSemesterFragmentToAddSemesterCoursesFragment())
-
-
+        //findNavController().navigate(BeginSemesterFragmentDirections.actionBeginSemesterFragmentToAddSemesterCoursesFragment())
+        setUpRv()
+        setupBarChart()
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    private fun setUpRv() {
+
+    }
+
+    private fun setupBarChart() {
+        val barChart = binding.readingProgressbarChart as BarChart
+
+        val entries: ArrayList<BarEntry> = ArrayList()
+        entries.add(BarEntry(1f, 0.5f))
+        entries.add(BarEntry(2f, 1f))
+        entries.add(BarEntry(3f, 2f))
+        entries.add(BarEntry(4f, 3f))
+        entries.add(BarEntry(5f, 4f))
+        entries.add(BarEntry(6f, 5f))
+
+        val barDataSet = BarDataSet(entries, "Cells")
+
+
+        val labels = ArrayList<String>()
+        //the first label is ignored.
+        labels.add("CPE 511")
+        labels.add("GRE 312")
+        labels.add("CPE513")
+        labels.add("CPE514")
+        labels.add("CPE518")
+        labels.add("ELE514")
+        labels.add("ELE514")
+
+
+        barChart.labelFor
+
+        val data = BarData(barDataSet)
+        barChart.data = data // set the data and list of labels into chart
+        Description().text = ("Testing here") // set the description
+        barDataSet.setColors(*ColorTemplate.LIBERTY_COLORS)
+        barChart.animateY(5000)
+
+        val xAxis: XAxis = barChart.xAxis
+        xAxis.valueFormatter = object : ValueFormatter() {
+            override fun getFormattedValue(value: Float): String {
+                return labels[value.toInt()]
+            }
+        }
     }
 
 
