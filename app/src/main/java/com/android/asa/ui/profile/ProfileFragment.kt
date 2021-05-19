ede8b7d401f6e6e49ca1957af4a5a6430e00bc22
Emmanuel Enya <enyasonjnr@gmail.com>
2021-05-19 18:08:12 +0100

    code clean up

--- app/src/main/java/com/android/asa/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/android/asa/ui/profile/ProfileFragment.kt
@@ -5,19 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.asa.R
 import com.android.asa.databinding.FragmentProfileBinding
 import com.android.asa.databinding.LayoutItemForUserCoursesBinding
+import com.android.asa.ui.auth.AuthViewModel
+import com.android.asa.ui.common.BaseFragment
 import com.android.asa.ui.widget.RecyclerViewAdapter
 import com.android.asa.ui.widget.ViewHolder
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class ProfileFragment : Fragment() {
+class ProfileFragment : BaseFragment() {
 
     private lateinit var binding: FragmentProfileBinding
+
+    private val viewModel by activityViewModels<ProfileViewModel>()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         binding = FragmentProfileBinding.inflate(layoutInflater)
@@ -29,16 +37,31 @@ class ProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setUpRecycler()
+        setUpClickListeners()
 
+    }
+
+    fun setUpClickListeners(){
+        binding.editProfile.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_editProfileFragment)
+        }
+
+        binding.backArrow.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
     private fun setUpRecycler() {
         binding.coursesRecyclerView.apply {
             adapter = coursessAdapter
-            addItemDecoration(CoursesItemDecorator(3,40,true))
-            layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.HORIZONTAL, false)
+//            addItemDecoration(CoursesItemDecorator(3,40,true))
+            layoutManager = GridLayoutManager(requireContext(),3,LinearLayoutManager.VERTICAL,false)
         }
 
+        coursessAdapter.submitList(viewModel.coursesList)
+
     }
+
+
 
     private val coursessAdapter =
             object : RecyclerViewAdapter<UserCourses>(
--- app/src/main/java/com/android/asa/ui/profile/ProfileViewHolder.kt
+++ app/src/main/java/com/android/asa/ui/profile/ProfileViewHolder.kt
@@ -1,13 +1,16 @@
 package com.android.asa.ui.profile
 
+import android.graphics.Color
 import com.android.asa.databinding.LayoutItemForUserCoursesBinding
 import com.android.asa.ui.widget.ViewHolder
 
 class ProfileViewHolder (private val binding: LayoutItemForUserCoursesBinding) :
         ViewHolder<UserCourses>(binding){
 
-
+    val cardBackgroundColors = listOf<String>("#00BBBA","#72ED77","#FFAD00","#EB5757","#BB6BD9","#4F4F4F")
     override fun bind(element: UserCourses) {
+        val randomColorIndex = (0..5).random()
+        binding.root.setCardBackgroundColor(Color.parseColor(cardBackgroundColors[randomColorIndex]))
         binding.courseCode.text = element.courseCode
         binding.courseProgress.text = element.CourseProgress
     }
