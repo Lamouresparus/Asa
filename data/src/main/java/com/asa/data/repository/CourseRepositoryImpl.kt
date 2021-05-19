75d6b9d35d2e9288390ccd810e835fcdb65b3cf3
Emmanuel Enya <enyasonjnr@gmail.com>
2021-05-19 07:46:38 +0100

    increment the noOfCoursesOffered locally when a course was successfully added

--- data/src/main/java/com/asa/data/repository/CourseRepositoryImpl.kt
+++ data/src/main/java/com/asa/data/repository/CourseRepositoryImpl.kt
@@ -1,18 +1,34 @@
 package com.asa.data.repository
 
+import com.asa.data.sharedPreference.SharedPreferenceReader
+import com.asa.data.sharedPreference.SharedPreferenceWriter
 import com.asa.data.sources.DataSourceFactory
 import com.asa.domain.AddCourseUseCase
 import com.asa.domain.model.CourseDomain
+import com.asa.domain.model.SemesterDomain
 import com.asa.domain.repository.CourseRepository
 import io.reactivex.Completable
 import io.reactivex.Single
 import javax.inject.Inject
 
 class CourseRepositoryImpl @Inject constructor(
-        private val dataSource: DataSourceFactory,
+    private val dataSource: DataSourceFactory,
+    private val prefWriter: SharedPreferenceWriter,
+    private val prefReader: SharedPreferenceReader
 ) : CourseRepository {
     override fun saveCourses(params: AddCourseUseCase.Params): Completable {
-        return dataSource.remote().saveCourses(params)
+        return dataSource.remote().saveCourses(params).doOnComplete {
+            /**
+             * increment the noOfCoursesOffered locally when a course was successfully added
+             */
+            val semesterInfo = prefReader.getSemesterInformation()
+            val updatedSemesterInfo =
+                SemesterDomain(
+                    semesterInfo.hasSemesterBegun,
+                    semesterInfo.noOfCoursesOffered + 1
+                )
+            prefWriter.saveSemesterInformation(updatedSemesterInfo)
+        }
     }
 
 
