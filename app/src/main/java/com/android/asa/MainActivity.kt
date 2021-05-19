7c1e75875df90377bd0a7ae4961587fa087e6030
Emmanuel Enya <enyasonjnr@gmail.com>
2021-05-19 09:49:50 +0100

    update start destination implementation with bottom bar

--- app/src/main/java/com/android/asa/MainActivity.kt
+++ app/src/main/java/com/android/asa/MainActivity.kt
@@ -7,13 +7,18 @@ import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.android.asa.databinding.ActivityMainBinding
+import com.asa.data.sharedPreference.SharedPreferenceReader
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+
+    @Inject
+    lateinit var prefReader: SharedPreferenceReader
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +28,32 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupViews() {
-        //Getting the Navigation Controller
         navController = Navigation.findNavController(this, R.id.nav_container)
-        //Setting the navigation controller to Bottom Nav
+
+        with(prefReader.getSemesterInformation()) {
+            val navGraph = if (hasSemesterBegun && noOfCoursesOffered > 0) {
+                binding.buttomNavigation.inflateMenu(R.menu.home_navigation_menu)
+                navController.navInflater.inflate(R.navigation.home_nav_graph).apply {
+                    startDestination = R.id.homeFragment
+                }
+            } else {
+                if (!hasSemesterBegun) {
+                    binding.buttomNavigation.inflateMenu(R.menu.home_navigation_menu_2)
+                    navController.navInflater.inflate(R.navigation.home_nav_graph).apply {
+                        startDestination = R.id.beginSemesterFragment
+                    }
+                } else {
+                    binding.buttomNavigation.inflateMenu(R.menu.home_navigation_menu_3)
+                    navController.navInflater.inflate(R.navigation.home_nav_graph).apply {
+                        startDestination = R.id.addSemesterCoursesFragment
+                    }
+                }
+            }
+            navController.graph = navGraph
+        }
+
         binding.buttomNavigation.setupWithNavController(navController)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/android/asa/ui/add_course/ViewAllCoursesAddedFragment.kt
+++ app/src/main/java/com/android/asa/ui/add_course/ViewAllCoursesAddedFragment.kt
@@ -1,15 +1,15 @@
 package com.android.asa.ui.add_course
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.navOptions
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.android.asa.R
+import com.android.asa.MainActivity
 import com.android.asa.databinding.FragmentAddAllCoursesBinding
 import com.android.asa.extensions.showToast
 import com.android.asa.ui.common.BaseFragment
@@ -24,8 +24,8 @@ class ViewAllCoursesAddedFragment : BaseFragment() {
     private val viewModel by activityViewModels<AddCoursesViewModel>()
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?,
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View {
         binding = FragmentAddAllCoursesBinding.inflate(layoutInflater)
         setUpViews()
@@ -39,10 +39,10 @@ class ViewAllCoursesAddedFragment : BaseFragment() {
 
     private fun setUpRecyclerViews() {
         binding.courseOfferedRv
-                .apply {
-                    layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
-                    adapter = allCoursesAdapter
-                }
+            .apply {
+                layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+                adapter = allCoursesAdapter
+            }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -79,9 +79,13 @@ class ViewAllCoursesAddedFragment : BaseFragment() {
             findNavController().popBackStack()
         }
         binding.doneButton.setOnClickListener {
-            findNavController()
-                    .navigate(ViewAllCoursesAddedFragmentDirections.actionAddAllCoursesFragment2ToHomeFragment(),
-                            navOptions { popUpTo = R.id.homeFragment })
+            requireActivity().apply {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+//            findNavController()
+//                .navigate(ViewAllCoursesAddedFragmentDirections.actionAddAllCoursesFragment2ToHomeFragment(),
+//                    navOptions { popUpTo = R.id.homeFragment })
         }
     }
 
