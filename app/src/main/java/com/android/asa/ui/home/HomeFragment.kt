9edda3d57d43ab7679be752ed9d895853cedfa4f
Love O'tudor <loveotudor@gmail.com>
2021-07-07 09:22:54 +0100

    updated code

--- app/src/main/java/com/android/asa/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android/asa/ui/home/HomeFragment.kt
@@ -104,6 +104,10 @@ class HomeFragment : BaseFragment() {
         binding.profilePhotoIv.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
         }
+
+        binding.notifications.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_addAllCoursesFragment2)
+        }
     }
 
     private fun setupBarChart() {
